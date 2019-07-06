6fda105671062ea2187ef719187fcd5a556de8c0
Christian Arévalo <carevaloq87@hotmail.com>
2019-07-06 22:32:30 +1000

    Actualizar datos y template para 2019

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,16 +1,15 @@
 var HomeView = function(store) {
- 
+
 	this.findById = function() {
-	    store.findById($('#text_date').val(), function(vencimientos) {
-	        console.log(vencimientos);
-	        $("h1").html(vencimientos.text);
-	    });
+		store.findById($('#text_date').val(), function(vencimientos) {
+			console.log(vencimientos);
+			$("h1").html(vencimientos.text);
+		});
 	};
 
     this.initialize = function() {
         $(document).on("keyup", "#text_date", this.findById);
     };
- 
+
     this.initialize();
 }
- 
